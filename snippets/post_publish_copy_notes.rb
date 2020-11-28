07de72aa9874ab78fcaa16556062db2d933aface
MBcom <27956078+MBcom@users.noreply.github.com>
2020-11-28 19:04:02 +0100

    add copy notes post publish script

---
+++ snippets/post_publish_copy_notes.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/ruby
+# encoding: UTF-8
+
+#
+# BigBlueButton open source conferencing system - http://www.bigbluebutton.org/
+#
+# Copyright (c) 2012 BigBlueButton Inc. and by respective authors (see below).
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 3.0 of the License, or (at your option)
+# any later version.
+#
+# BigBlueButton is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with BigBlueButton; if not, see <http://www.gnu.org/licenses/>.
+#
+
+require "trollop"
+require File.expand_path('../../../lib/recordandplayback', __FILE__)
+
+opts = Trollop::options do
+  opt :meeting_id, "Meeting id to archive", :type => String
+end
+meeting_id = opts[:meeting_id]
+
+logger = Logger.new("/var/log/bigbluebutton/post_publish.log", 'weekly' )
+logger.level = Logger::INFO
+BigBlueButton.logger = logger
+
+
+#
+# Put your code here
+#
+
+BigBlueButton.logger.info("Copy notes for [#{meeting_id}] if available")
+rs = `/usr/local/bigbluebutton/core/scripts/post_publish/copy_notes.sh #{meeting_id}`
+BigBlueButton.logger.info(rs)
+BigBlueButton.logger.info("Copy notes for [#{meeting_id}] end")
+exit 0
